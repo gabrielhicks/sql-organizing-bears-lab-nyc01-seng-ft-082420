113b00c8662b201adc37d58d5b72ee53d158224f
gabrielhicks <gabrielhicks@mail.com>
2020-09-01 15:29:04 -0400

    SQL IS OKAY

---
+++ lib/insert.sql
@@ -0,0 +1,64 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ( "Sally",
+        10,
+        "F",
+        "black",
+        "angry",
+        true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("jess",
+        1,
+        "F",
+        "brown",
+        "goofy",
+        true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("gabe",
+        8,
+        "M",
+        "black",
+        "goofy",
+        true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (null,
+        2,
+        "F",
+        "brown",
+        "goofy",
+        true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("seyi",
+        5,
+        "M",
+        "black",
+        "angry",
+        true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("ronald",
+        7,
+        "M",
+        "brown",
+        "goofy",
+        true );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Cindy",
+        11,
+        "F",
+        "black",
+        "angry",
+        false );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Emily",
+        4,
+        "F",
+        "black",
+        "goofy",
+        true );
+
